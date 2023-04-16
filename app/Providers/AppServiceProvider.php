328f46f4bdc2e4deb61f6efda892929f8ab45178
Rapynets Vasyl <rapinecvasiliy@gmail.com>
2023-04-16 10:08:49 +0300

    add livewire package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        // DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
+        //     // Notify development team...
+        // });
+
+        // TODO request cycle
     }
 }
