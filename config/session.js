bb6178a7725f81fba31481f350a20dc41ef90f2d
Vasyl Rapynets <rapinecvasiliy@gmail.com>
2026-02-22 17:03:03 +0200

    change session config file

--- config/session.js
+++ config/session.js
@@ -5,11 +5,6 @@ const sessionConfig = session({
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    maxAge: 1000 * 60 * 60
-  }
 })
 
 export default sessionConfig
