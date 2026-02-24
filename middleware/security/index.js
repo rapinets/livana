f3fcce72e88adb05ce59d7b3f133cfd1400ca941
Vasyl Rapynets <rapinecvasiliy@gmail.com>
2026-02-24 16:56:36 +0200

    change allowedOrigins

--- middleware/security/index.js
+++ middleware/security/index.js
@@ -61,7 +61,6 @@ export function applySecurity(app, opts = {}) {
       'http://localhost:5500',
       'http://localhost:8080',
       'http://localhost:5173',
-      'https://livana-1.onrender.com',
     ],
     corsAllowNoOrigin = true,
     corsCredentials = true,
