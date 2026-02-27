2e9b7979ad1e24d8f79b00a2353d8c4bae2f7c8e
Vasyl Rapynets <rapinecvasiliy@gmail.com>
2026-02-27 16:37:39 +0200

    change user validator

--- src/v1/validators/user/userValidator.js
+++ src/v1/validators/user/userValidator.js
@@ -36,51 +36,13 @@ export const loginSchema = z.object({
 })
 
 export function validationUser(schema) {
-  return async (req, res, next) => {
-    try {
-      const email = req.session.registerEmail || ''
-
-      if (!email) {
-        return res.redirect('/users/register')
-      }
-
-      const type = await TypesDBService.getOne('user')
-
-      const safeBody = {
-        name: req.body.name ?? '',
-        password: req.body.password ?? '',
-        email,
-        type: type._id
-      }
-
-      if (req.file?.filename) {
-        safeBody.img = req.file.filename
-      }
-
-      const result = schema.safeParse(safeBody)
-
-      if (!result.success) {
-        const errors = {}
-        result.error.issues.forEach(issue => {
-          const field = issue.path[0]
-          if (!errors[field]) {
-            errors[field] = issue.message
-          }
-        })
-
-        return res.render('users/register', {
-          data: req.body,
-          user: req.user,
-          errors,
-          email
-        })
-      }
-
-      req.validated = result.data
-      next()
-
-    } catch (err) {
-      res.status(500).json({ error: err.message })
+  return (req, res, next) => {
+    const result = schema.safeParse(req.body)
+    if (!result.success) {
+      const errors = result.error.issues.map((err) => err.message)
+      return res.status(400).json({ errors })
     }
+    req.validated = result.data
+    next()
   }
 }
